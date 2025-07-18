cb6cd6550a7a6bf57fbc917db15b4f7401c4623c
SaloniPawar-59 <pawarsalo0509@gmail.com>
2025-07-18 17:56:08 +0530

    Create main.tf

---
+++ modules /ec2/main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "ec2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  security_groups = [var.security_group_id]
+  key_name      = var.key_name
+  associate_public_ip_address = var.associate_public_ip
+  user_data = file(var.user_data_file)
+
+  tags = { Name = var.name }
+}
